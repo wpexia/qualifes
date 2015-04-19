38a5b19689822bc45b5639b54c72e21a50ac31fc
wpexia <wpexia@gmail.com>
2015-04-19 21:58:28 +0800

    浏览历史OK

---
+++ src/com/qualifies/app/UI/adapter/ProductListAdapter.java
@@ -0,0 +1,77 @@
+package com.qualifies.app.ui.adapter;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.qualifies.app.R;
+import com.qualifies.app.util.DownloadImageTask;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ProductListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater = null;
+    private List<HashMap<String, Object>> mData;
+    private boolean hasStar = false;
+
+    public ProductListAdapter(Context context, List<HashMap<String, Object>> data, boolean star) {
+        this(context, data);
+        hasStar = star;
+    }
+
+    public ProductListAdapter(Context context, List<HashMap<String, Object>> data) {
+        mInflater = LayoutInflater.from(context);
+        mData = data;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null)
+            convertView = mInflater.inflate(R.layout.history_item, null);
+        ImageView image = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView place = (TextView) convertView.findViewById(R.id.place);
+        TextView discount = (TextView) convertView.findViewById(R.id.discount);
+        TextView price = (TextView) convertView.findViewById(R.id.price);
+        TextView oldPrice = (TextView) convertView.findViewById(R.id.oldPrice);
+        ImageView star = (ImageView) convertView.findViewById(R.id.star);
+        if (hasStar)
+            star.setImageResource(R.drawable.star_red);
+        title.setText(mData.get(position).get("title").toString());
+        place.setText(mData.get(position).get("place").toString());
+        discount.setText(mData.get(position).get("discount").toString());
+        price.setText(mData.get(position).get("price").toString());
+        oldPrice.setText(mData.get(position).get("oldPrice").toString());
+        oldPrice.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.FAKE_BOLD_TEXT_FLAG);
+        new DownloadImageTask(image).execute(mData.get(position).get("image").toString());
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+
+    public void delete(int position) {
+        mData.remove(position);
+        notifyDataSetChanged();
+    }
+
+}
--- src/com/qualifies/app/UI/personal/HistoryActivity.java
+++ src/com/qualifies/app/UI/personal/HistoryActivity.java
@@ -4,22 +4,44 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.qualifies.app.R;
+import com.qualifies.app.ui.adapter.ProductListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HistoryActivity extends Activity {
     private SwipeMenuListView listView;
+    private ProductListAdapter historyAdapter;
+    private List<HashMap<String, Object>> data;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+        initView();
+    }
+
+    private void initView() {
         listView = (SwipeMenuListView) findViewById(R.id.listView);
+        createSwipeMenu();
+        data = getData();
+        historyAdapter = new ProductListAdapter(this, data);
+        listView.setAdapter(historyAdapter);
+        listView.setDividerHeight(0);
+        text = (TextView)findViewById(R.id.title);
+        text.setText("浏览历史");
+    }
 
-
+    private void createSwipeMenu() {
         SwipeMenuCreator creator = new SwipeMenuCreator() {
 
             @Override
@@ -28,13 +50,36 @@ public class HistoryActivity extends Activity {
                         getApplicationContext());
                 deleteItem.setBackground(new ColorDrawable(Color.rgb(0xFF, 0x33,
                         0x00)));
-                deleteItem.setWidth(90);
-                deleteItem.setTitle("Open");
+                deleteItem.setWidth(140);
+                deleteItem.setTitle("删除");
                 deleteItem.setTitleSize(18);
                 deleteItem.setTitleColor(Color.WHITE);
                 menu.addMenuItem(deleteItem);
             }
         };
         listView.setMenuCreator(creator);
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                Log.w("SwipeMenuListView", "OnClick" + String.valueOf(position) + "    " + String.valueOf(index));
+                historyAdapter.delete(position);
+                return true;// false : not close the menu; true : close the menu
+            }
+        });
     }
+
+    private List<HashMap<String, Object>> getData() {
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        for (int i = 1; i <= 40; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("title", "婴儿神仙水 Mommy bilss gripe 缓解 胀气 吐奶等");
+            map.put("image", "http://test.qualifes.com/images/201410/thumb_img/90_thumb_G_1414501935478.jpg");
+            map.put("place", "产地 美国" + String.valueOf(i));
+            map.put("discount", "55% 折扣");
+            map.put("price", "￥298.00");
+            map.put("oldPrice", "￥589.00");
+            list.add(map);
+        }
+        return list;
+    }
+
 }
