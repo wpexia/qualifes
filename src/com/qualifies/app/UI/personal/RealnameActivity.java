43c31575b41f056e8d9610f6fdf30a7b5046a764
U-USER-20150406LM\Administrator <Administrator@USER-20150406LM.(none)>
2015-04-22 14:23:28 +0800

    实名认证

---
+++ src/com/qualifies/app/UI/personal/RealnameActivity.java
@@ -0,0 +1,56 @@
+package com.qualifies.app.ui.personal;
+
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import com.qualifies.app.R;
+import com.qualifies.app.ui.fragment.RealNameNULLFragment;
+import com.qualifies.app.ui.fragment.RealNameOKFragment;
+
+public class RealNameActivity extends Activity {
+
+
+    private FragmentManager manager;
+    private SharedPreferences sp;
+    private RealNameNULLFragment realNameNULLFragment;
+    private RealNameOKFragment realNameOKFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.realname);
+        manager = getFragmentManager();
+        sp = getSharedPreferences("User", MODE_PRIVATE);
+        initView();
+    }
+
+    private void initView() {
+        FragmentTransaction transaction = manager.beginTransaction();
+        hideFragment(transaction);
+        if (sp.contains("realName")) {
+            if (realNameOKFragment == null) {
+                realNameOKFragment = new RealNameOKFragment();
+                transaction.add(R.id.content, realNameOKFragment);
+            } else
+                transaction.show(realNameOKFragment);
+        } else {
+            if (realNameNULLFragment == null) {
+                realNameNULLFragment = new RealNameNULLFragment();
+                transaction.add(R.id.content, realNameNULLFragment);
+            }
+        }
+        transaction.commit();
+    }
+
+    private void hideFragment(FragmentTransaction transaction) {
+        if (realNameNULLFragment != null) {
+            transaction.hide(realNameNULLFragment);
+        }
+        if (realNameOKFragment != null) {
+            transaction.hide(realNameOKFragment);
+        }
+    }
+}
