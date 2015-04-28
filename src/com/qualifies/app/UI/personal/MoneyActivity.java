1395494b1faa034be4a3c0eb1459f384b63ef815
wpexia <wpexia@gmail.com>
2015-04-28 23:42:27 +0800

    pause

---
+++ src/com/qualifies/app/UI/personal/MoneyActivity.java
@@ -0,0 +1,13 @@
+package com.qualifies.app.ui.personal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.qualifies.app.R;
+
+public class MoneyActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.money);
+    }
+}
