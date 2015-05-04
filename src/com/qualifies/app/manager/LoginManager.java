3c6a47d8981b09dee7265371dc7790b832ab1dd4
wpexia <wpexia@gmail.com>
2015-05-04 15:21:39 +0800

    暂时还未解决

--- src/com/qualifies/app/manager/LoginManager.java
+++ src/com/qualifies/app/manager/LoginManager.java
@@ -4,17 +4,18 @@ package com.qualifies.app.manager;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Base64;
 import android.util.Log;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.SyncHttpClient;
 import com.qualifies.app.config.Api;
-import com.qualifies.app.util.RSAUtils;
+import com.qualifies.app.util.Base64Helper;
+import com.qualifies.app.util.RsaHelper;
 import org.apache.http.Header;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
@@ -34,32 +35,23 @@ public class LoginManager {
 
     public void login(String username, String password, Handler handler, Context context) {
         this.username = username;
-        this.password = password;
-        this.handler = handler;
-        this.context = context;
         try {
-            InputStream publicIn = context.getAssets().open("public_key.pem");
-            PublicKey publicKey = RSAUtils.loadPublicKey(publicIn);
-            byte[] encryptByte = RSAUtils.encryptData(password.getBytes(), publicKey);
-            String afterencrypt = Base64.encodeToString(encryptByte, Base64.NO_PADDING);
-            this.password = afterencrypt;
-            Log.e("encode", afterencrypt);
-            publicIn.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+            Log.e("raw", password);
+            InputStream publicKeyIn = context.getAssets().open("rsa_public_key.pem");
+            PublicKey publicKey = RsaHelper.loadPublicKey(publicKeyIn);
+            this.password = RsaHelper.encryptDataFromStr(password, publicKey);
+            this.password = URLEncoder.encode(this.password, "UTF-8");
+            Log.e("password", this.password);
 
-        try {
-            InputStream privateIn = context.getAssets().open("private_key.pem");
-            PrivateKey privateKey = RSAUtils.loadPrivateKey(privateIn);
-            byte[] decodeByte = RSAUtils.decryptData(Base64.decode(this.password, Base64.NO_PADDING), privateKey);
-            String afterdecrypy = Base64.encodeToString(decodeByte, Base64.NO_PADDING);
-            Log.e("test   ", afterdecrypy);
+
+            InputStream privateKeyIn = context.getAssets().open("pkcs8_rsa_private_key.pem");
+            PrivateKey privateKey = RsaHelper.loadPrivateKey(privateKeyIn);
+            Log.e("decode", new String(RsaHelper.decryptData(Base64Helper.decode(this.password),privateKey)));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
+        this.handler = handler;
+        this.context = context;
         Thread loginThread = new Thread(new LoginThread());
         loginThread.start();
     }
@@ -73,7 +65,11 @@ public class LoginManager {
             final Message msg = handler.obtainMessage();
             requestParams.put("_type", "log");
             requestParams.put("phone", username);
-            requestParams.put("pwd", password);
+            try {
+                requestParams.put("pwd", password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             client.post(Api.url(service), requestParams, new JsonHttpResponseHandler() {
 
                 @Override
